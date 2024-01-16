8ebcfbf9ca87d120db3bd01d24b519a24273c979
mathiaswamucani <mathiaswamucani.mw@gmail.com>
2024-01-16 18:16:16 +0300

    Added dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,88 +2,30 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
-use App\Http\Requests\Auth\LoginRequest;
-use App\Models\User;
-use App\Providers\RouteServiceProvider;
-use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use App\Classes\GeneralHelper;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Response;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Api\ApiDashboardController;
 
 class DashboardController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
-    // use AuthenticatesUsers;
-    // use AuthenticatesUsers {
-    //     logout as performLogout;
-    // }
 
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
-    // protected $redirectTo = RouteServiceProvider::DASHBOARD;
+    public $module_path;
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
-        $this->middleware('guest')->except('logout');
+        // $this->module_path = GeneralHelper::DashboardPath('');
     }
 
-    public function index(Request $request){
-
-        return  Response::json(['res'=>'hello world']);
-    }
-
-    protected function credentials(Request $request)
+    public function index()
     {
-        return [
-            'email' => request()->email,
-            'password' => request()->password,
-            // 'active' => 1
-        ];
-    }
+        // $data = [];
+        // $data['dashboardIndicators']    = ApiDashboardController::dashboard();
+        // $data['projectProgress']        = DB::table('view_dashboard_proj_progress_totals')->get();
+        // $data['newSources']             = DB::table('view_dashboard_proj_progress_breakdown')->get();
+        // return '<pre>'.json_encode($data['dashboardIndicators']->getdata(), JSON_PRETTY_PRINT);//->access->bgcolor;
 
-    protected function authenticated()
-    {
-        /**
-         * Check if user email is verified
-         */
-        if (!Auth::user()->email_verified_at) {
-            Auth::logout();
-            return redirect('login')->withErrors(['Please verify your email']);
-        }
-
-        /**
-         * Check if user account is active
-         */
-        // else if (!Auth::user()->active) {
-        //     Auth::logout();
-        //     return redirect('login')->withErrors(['Your account is inactive']);
-        // }
-
-        // else if(auth()->user()){
-
-        // }
-    }
-
-    protected function loggedOut(Request $request)
-    {
-        return redirect()->route('home');
+        return view('dashboard.index');
+        // return view($this->module_path . '.dashboard', $data);
     }
 }
