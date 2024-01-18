5cbc75f815801fb85f5aa5eb0aec534757a360ea
mathiaswamucani <mathiaswamucani.mw@gmail.com>
2024-01-18 18:34:53 +0300

    Added old persons form

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -20,30 +20,25 @@ class DashboardController extends Controller
 
     public function index()
     {
-        // $data = [];
-        // $data['dashboardIndicators']    = ApiDashboardController::dashboard();
-        // $data['projectProgress']        = DB::table('view_dashboard_proj_progress_totals')->get();
-        // $data['newSources']             = DB::table('view_dashboard_proj_progress_breakdown')->get();
-        // return '<pre>'.json_encode($data['dashboardIndicators']->getdata(), JSON_PRETTY_PRINT);//->access->bgcolor;
-        // return print_r($this->globalFunc());
-        $client = new Client();
-        $res = $client->request('POST', 'http://test-nsr.mglsd.go.ug:20190/connect/token', [
-            'headers' => ['Content-Type' => 'application/json'],
-            'json' => [
+       
+        // $client = new Client();
+        // $res = $client->request('POST', 'http://test-nsr.mglsd.go.ug:20190/connect/token', [
+        //     'headers' => ['Content-Type' => 'application/json'],
+        //     'json' => [
 
-                "grant_type" => "password",
-                "username" => "centricsolutionslimited@gmail.com",
-                "password" => "247@Centric?"
-            ]
-        ]);
+        //         "grant_type" => "password",
+        //         "username" => "centricsolutionslimited@gmail.com",
+        //         "password" => "247@Centric?"
+        //     ]
+        // ]);
         // echo $res->getStatusCode();
         // 200
         // echo $res->getHeader('content-type');
         // 'application/json; charset=utf8'
         // echo $res->getBody();
-        $jsonResponseToken =  json_decode($res->getBody()->getContents(), true);
+        // $jsonResponseToken =  json_decode($res->getBody()->getContents(), true);
 
-        $accessToken =  $jsonResponseToken['access_token'];
+        // $accessToken =  $jsonResponseToken['access_token'];
 
         // $adminLists = $client->request('GET', 'http://test-nsr.mglsd.go.ug:20190/api/v1/getGeoLocationsMasterList', [
         //     'headers' => ['Content-Type' => 'application/json', 'Authorization' => "Bearer {$accessToken}"],
@@ -59,6 +54,5 @@ class DashboardController extends Controller
         // return $adminsRes;
 
         return $this->view('dashboard.index');
-        // return view($this->module_path . '.dashboard', $data);
     }
 }
