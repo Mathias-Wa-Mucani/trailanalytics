1012477d50fc5c8e1a498340e451809837a03839
ELLY YIGA <ellyyiga@gmail.com>
2024-01-22 10:04:06 +0300

    ui

--- resources/views/elements/toolbar.blade.php
+++ resources/views/elements/toolbar.blade.php
@@ -13,7 +13,7 @@
                     <span class="h-20px border-gray-200 border-start ms-3 mx-2"></span>
                     <!--end::Separator-->
                     <!--begin::Description-->
-                    <small class="text-muted fs-7 fw-semibold my-1 ms-1">#XRS-45670</small>
+                    <small class="text-muted fs-7 fw-semibold my-1 ms-1">#</small>
                     <!--end::Description-->
                 </h1>
                 <!--end::Title-->
@@ -23,45 +23,8 @@
         <!--end::Page title-->
         <!--begin::Action group-->
         <div class="d-flex align-items-center flex-wrap">
-            <!--begin::Wrapper-->
-            <div class="flex-shrink-0 me-2">
-                <ul class="nav">
-                    <li class="nav-item">
-                        <a class="nav-link btn btn-sm btn-color-muted btn-active-color-primary btn-active-light active fw-semibold fs-7 px-4 me-1"
-                            data-bs-toggle="tab" href="#">Day</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link btn btn-sm btn-color-muted btn-active-color-primary btn-active-light fw-semibold fs-7 px-4 me-1"
-                            data-bs-toggle="tab" href="">Week</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link btn btn-sm btn-color-muted btn-active-color-primary btn-active-light fw-semibold fs-7 px-4"
-                            data-bs-toggle="tab" href="#">Year</a>
-                    </li>
-                </ul>
-            </div>
-            <!--end::Wrapper-->
-            <!--begin::Wrapper-->
-            <div class="d-flex align-items-center">
-                <!--begin::Daterangepicker-->
-                <a href="#"
-                    class="btn btn-sm btn-bg-light btn-color-gray-500 btn-active-color-primary me-2"
-                    id="kt_dashboard_daterangepicker" data-bs-toggle="tooltip" data-bs-dismiss="click"
-                    data-bs-trigger="hover" title="Select dashboard daterange">
-                    <span class="fw-semibold me-1" id="kt_dashboard_daterangepicker_title">Range:</span>
-                    <span class="fw-bold" id="kt_dashboard_daterangepicker_date">December 28</span>
-                </a>
-                <!--end::Daterangepicker-->
-                <!--begin::Actions-->
-                <div class="d-flex align-items-center me-n2 me-lg-0">
-                    <button type="button"
-                        class="btn btn-sm btn-icon btn-color-primary btn-active-light btn-active-color-primary"
-                        data-bs-toggle="modal" data-bs-target="#kt_modal_create_campaign">
-                        <i class="ki-outline ki-plus-square fs-2"></i>
-                    </button>
-                </div>
-                <!--end::Actions-->
-            </div>
+            
+            
             <!--end::Wrapper-->
         </div>
         <!--end::Action group-->
