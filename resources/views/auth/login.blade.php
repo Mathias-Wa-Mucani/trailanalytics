61abc906150284b289965284cd0b0927ca00b956
mathiaswamucani <mathiaswamucani.mw@gmail.com>
2024-02-14 13:47:54 +0300

    Added login with google

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -177,7 +177,7 @@
                         <!--end::Submit button-->
                         <!--begin::Sign up-->
                         <div class="text-gray-500 text-center fw-semibold fs-6">Not a Member yet?
-                            <a href="authentication/layouts/creative/sign-up.html" class="link-primary">Sign up</a>
+                            <a href="#" class="link-primary">Sign up</a>
                         </div>
                         <!--end::Sign up-->
                     </form>
